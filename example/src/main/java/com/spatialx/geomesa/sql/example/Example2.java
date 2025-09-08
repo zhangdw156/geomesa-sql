6e02772a42195dbaf27674fbd0809d44a091707e
zhangdw <zhangdawei24@mails.ucas.ac.cn>
2025-09-08 10:42:19 +0800

    change sql

--- example/src/main/java/com/spatialx/geomesa/sql/example/Example2.java
+++ example/src/main/java/com/spatialx/geomesa/sql/example/Example2.java
@@ -16,7 +16,8 @@ public class Example2 {
         info.setProperty("caseSensitive", "false");
         try (Connection connection = DriverManager.getConnection(jdbcUrl, info)) {
             System.out.println("成功连接");
-            String sql = "select * from beijing_subway where name = 'S1线'";
+//            String sql = "explain plan for SELECT line, COUNT(1) FROM beijing_subway_station JOIN beijing_subway ON ST_Intersects(beijing_subway.geom, beijing_subway_station.geom) GROUP BY line ";
+            String sql = "explain plan for SELECT line, COUNT(1) FROM beijing_subway_station where line LIKE '%线' GROUP BY line";
             try (Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(sql)) {
                 int numColumns = resultSet.getMetaData().getColumnCount();
